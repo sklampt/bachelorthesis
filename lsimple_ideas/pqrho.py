16de664647db5b3799b31685d69bbac735d66d95
Sina Klampt <sklampt@student.ethz.ch>
2021-09-22 12:39:56 +0200

    update range rho_air

--- lsimple_ideas/pqrho.py
+++ lsimple_ideas/pqrho.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 rho_inv  = np.arange(1., 8.5, 0.05)
 plt.plot(rho_inv, label='pqrho from diagnostics')
 
-rho_air  = np.arange(0.25,1.47,0.0075)
+rho_air  = np.arange(0.15,1.47,0.0075)
 rho_inv_air = 1.3 / rho_air
 plt.plot(rho_inv_air, label='pqrho, calculated as: 1.3/rho_air')
 
