8fb67db752b2c7047de76df63127f395b5be7cf8
Sina Klampt <sklampt@student.ethz.ch>
2021-09-30 12:37:18 +0200

    resolve minor issue

--- scripts_plotting/histogram.py
+++ scripts_plotting/histogram.py
@@ -21,35 +21,27 @@ for i in range(len(parameter)):
 
 	d1  = Dataset('/net/n2o/wolke_scratch/sklampt/echam/test793_taylor_ws/test793_taylor/'.format(run_nr_diff)+'test793_taylor_year_200312.01_activ'.format(run_nr_diff)+'.nc')
 
-	# Access variables, e.g.
-	var = parameter[i]
-	if var == 'DIAG01_2':
-		unit = ' [m]'
-		var = 'ice_crystal_radius'
-	elif var == 'DIAG01_3':
-		unit = ' [kg m-3]'
-		var = 'rho_air'
-	else:
-		var = 'alpha'
 
 	run_name = '12_init'
 
-	plot_name= 'frequency_{}_{}'.format(var, run_name) # adjust name
-	plot_title = 'Frequency {}'.format(var)
-
 	fig, ax = plt.subplots(nrows=1, ncols=1)
 
-	#ris
-	if var == 'ice_crystal_radius':
+	if var == 'DIAG01_2':
 		#plt.hist(d1[var][:,:,:].flatten(), bins=[1e-7, 1e-6, 1e-5, 0.1, 0.9, 0.99, 1.], label='december')
 		#plt.hist(d1[var][:,:,:].flatten(), bins=[1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 0.1], label='december')
 		plt.hist(d1[var][:,:,:].flatten(), label='december')
-	elif var == 'rho_air':
+		var = 'ice_crystal_radius'
+		unit = ' [m]'
+	elif var == 'DIAG01_3':
 		plt.hist(d1[var][:,:,:].flatten(), label='december')
+		var = 'rho_air'
+		unit = ' [kg m-3]'
 	else:
 		plt.hist(d1[var][:,:,:].flatten(), label='december')
+		var = 'alpha'
 
-
+	plot_name= 'frequency_{}_{}'.format(var, run_name) # adjust name
+	plot_title = 'Frequency {}'.format(var)
 
 	plt.title(plot_title)
 
