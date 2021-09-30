58c29cb07506fb876b86eaa643283ecd031d4688
sklampt <sklampt@inf.ethz.ch>
2021-09-30 12:53:27 +0200

    update plot

--- scripts_plotting/histogram.py
+++ scripts_plotting/histogram.py
@@ -19,26 +19,26 @@ for i in range(len(parameter)):
 	run_nr = 1
 	run_nr_diff = 2
 
-	d1  = Dataset('/net/n2o/wolke_scratch/sklampt/echam/test793_taylor_ws/test793_taylor/'.format(run_nr_diff)+'test793_taylor_year_200312.01_activ'.format(run_nr_diff)+'.nc')
+	d1  = Dataset('/net/n2o/wolke_scratch/sklampt/echam/test793_taylor_ws/test793_taylor/'.format(run_nr_diff)+'test793_taylor_year_200301.01_activ'.format(run_nr_diff)+'.nc')
 
 	var = parameter[i]
 
-	run_name = '12_init'
+	run_name = '01_init'
 
 	fig, ax = plt.subplots(nrows=1, ncols=1)
 
 	if var == 'DIAG01_2':
 		#plt.hist(d1[var][:,:,:].flatten(), bins=[1e-7, 1e-6, 1e-5, 0.1, 0.9, 0.99, 1.], label='december')
 		#plt.hist(d1[var][:,:,:].flatten(), bins=[1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 0.1], label='december')
-		plt.hist(d1[var][:,:,:].flatten(), label='december')
+		plt.hist(d1[var][:,:,:].flatten(), label='january')
 		var = 'ice_crystal_radius'
 		unit = ' [m]'
 	elif var == 'DIAG01_3':
-		plt.hist(d1[var][:,:,:].flatten(), label='december')
+		plt.hist(d1[var][:,:,:].flatten(), label='january')
 		var = 'rho_air'
 		unit = ' [kg m-3]'
 	else:
-		plt.hist(d1[var][:,:,:].flatten(), label='december')
+		plt.hist(d1[var][:,:,:].flatten(), label='january')
 		var = 'alpha'
 
 	plot_name= 'frequency_{}_{}'.format(var, run_name) # adjust name
@@ -49,7 +49,7 @@ for i in range(len(parameter)):
 	plt.legend(bbox_to_anchor=(1,1), loc="upper left")#, title = 'sed. of cloud ice *') # loc decides location of legend
 	ax.set_ylabel('frequency')          #CDNC & ICNC [1/m2] not [1/m3]!
 	ax.set_xlabel(var + unit)
-	ax.set_xscale('log')
+	#ax.set_xscale('log')
 
 
-	plt.savefig('/net/n2o/wolke_scratch/sklampt/echam/plots/test793_taylor/'+'/plot_{}.pdf'.format(plot_name), bbox_inches='tight')
+	plt.savefig('/net/n2o/wolke_scratch/sklampt/echam/plots/test793_taylor/init/'+'/plot_{}.pdf'.format(plot_name), bbox_inches='tight')
