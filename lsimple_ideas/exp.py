6ed9c8b9025956b3dbad6e16c1db93c9ac2a1f19
sklampt <sklampt@student.ethz.ch>
2021-11-15 04:05:00 +0100

    script for exp comparison, exact - TSE

---
+++ lsimple_ideas/exp.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+r_iv = np.arange(8e-6, 1e-4, 1e-6)
+plt.plot(r_iv, np.log10(r_iv*1e4), label= 'log')
+
+def alpha1(r):
+    r_so = 1e-4 #m
+    x = (r - 1)/(r + 1)
+    logarithm = 2 * (x + x**3/3 + x**5/5)
+    alpha = logarithm/np.log(10)
+    return alpha
+
+result = np.zeros(len(r_iv))
+for i in range(len(r_iv)):
+    result[i] = alpha1(r_iv[i]*1e4)
+
+plt.plot(r_iv, result, label= 'taylor')
+
+plt.legend(bbox_to_anchor=(1,1), loc="upper left")
+plt.show()
