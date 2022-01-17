d99026db13b3d4b0a11906ad890b95d4f429b72e
sklampt <sklampt@student.ethz.ch>
2022-01-17 17:55:44 +0100

    format code

--- experiments/scripts/plotting/difference1.py
+++ experiments/scripts/plotting/difference1.py
@@ -29,8 +29,9 @@ file_name = ['test791_onlyaccr_taylor',
 			 'test793_176', 
 			 'test793_taylor']
 
-simplification_name = ['Accretion - Taylor', 
-					   'Accretion - double Taylor', 
+simplification_name = ['Accretion - Taylor applied in Collision Efficiency Formulation', 
+					   'Accretion - Taylor applied in Accretion Formulation',
+					   'Accretion - Taylor applied in both Formulations', 
 					   'Aggregration - Drastic, alpha = 156', 
 					   'Aggregation - Drastic, alpha = 176', 
 					   'Aggregation - Taylor']
